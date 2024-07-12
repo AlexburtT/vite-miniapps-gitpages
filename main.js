d4fbb3d784268ea64c7436dd7bb9dc4fb0921449
AlexburtT <alexburt12634@gmail.com>
2024-07-12 16:20:47 +0900

    update user

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 import './style.css'
 import { setupCounter } from './counter.js'
 
-window.Telegram.WebApp.ready()
-window.Telegram.WebApp.onEvent('mainButtonClicked', () => {
-  window.Telegram.WebApp.close()
-})
 
-const userName = window.Telegram.WebApp.initDataUnsafe.user?.username
-const userPhone = window.Telegram.WebApp.initDataUnsafe.user?.phone
-const userPlatform = window.Telegram.WebApp.initDataUnsafe.platform
-const userColor = window.Telegram.WebApp.initDataUnsafe.user?.color
+const userName = window.Telegram.WebApp.user?.first_name
+const userPhone = window.Telegram.WebApp.user?.phone
+const userUsername = window.Telegram.WebApp.user?.username
+const userPrem = window.Telegram.WebApp.user?.is_premium
 document.querySelector('#app').innerHTML = `
   <div>    
     <h1>Hello ${userName || 'anonymous'}</h1>
     <h2>Phone: ${userPhone || 'anonymous'}</h2>
-    <h2>Platform: ${userPlatform || 'anonymous'}</h2>
-    <h2>Color: ${userColor || 'anonymous'}</h2>
+    <h2>Platform: ${userUsername || 'anonymous'}</h2>
+    <h2>Color: ${userPrem || 'anonymous'}</h2>
     <h2>Telegram Web App</h2>
     <div class="card">
       <button id="counter" type="button"></button>
