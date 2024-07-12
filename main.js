38135d584cf48a7bb0813b04c9f3bca65bf0921a
AlexburtT <alexburt12634@gmail.com>
2024-07-12 16:48:42 +0900

    update js

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ const { initDataRaw, initData } = retrieveLaunchParams();
 console.log(initDataRaw, initData);
 
 const userName = initData?.user?.first_name
-const userPhone = window.Telegram.WebApp.user?.phone
+// const userPhone = window.Telegram.WebApp.user?.phone
 const userUsername = initDataRaw?.user?.username
-const userPrem = window.Telegram.WebApp.user?.is_premium
+// const userPrem = window.Telegram.WebApp.user?.is_premium
 document.querySelector('#app').innerHTML = `
   <div>    
     <h1>Hello ${userName || 'anonymous'}</h1>
-    <h2>Phone: ${userPhone || 'anonymous'}</h2>
+    
     <h2>Platform: ${userUsername || 'anonymous'}</h2>
-    <h2>Color: ${userPrem || 'anonymous'}</h2>
+    
     <h2>Telegram Web App</h2>
     <div class="card">
       <button id="counter" type="button"></button>
