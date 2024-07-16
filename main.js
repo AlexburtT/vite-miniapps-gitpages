de3c0d16701514feacc82f1de6df3ecdacf5e422
AlexburtT <alexburt12634@gmail.com>
2024-07-16 14:53:49 +0900

    update user

--- main.js
+++ main.js
@@ -4,14 +4,30 @@ import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
 
-const userPhone = WebApp.initDataUnsafe.user.first_name;
-const userUsername = WebApp.platform;
+const userFirstName = WebApp.initDataUnsafe.user.first_name;
+const userLastName = WebApp.initDataUnsafe.user.last_name;
+const userUserName = WebApp.initDataUnsafe.user.username;
+const userId = WebApp.initDataUnsafe.user.id;
+const userIsBot = WebApp.initDataUnsafe.user.is_bot;
+const userLanguageCode = WebApp.initDataUnsafe.user.language_code;
+const userIsPremium = WebApp.initDataUnsafe.user.is_premium;
+const userPhoto = WebApp.initDataUnsafe.user.photo;
+const userPlatform = WebApp.platform;
+
+console.log(WebApp);
 
 document.querySelector('#app').innerHTML = `
   <div>    
-    <h1>Hello ${userPhone || 'anonymous'}</h1>
+    <h1>Hello ${userFirstName || 'Пётр'}</h1>
     
-    <h2>Platform: ${userUsername || 'anonymous'}</h2>
+    <p>Platform: ${userLastName || 'Петровски'}</p>
+    <p>Platform: ${userUserName || 'Петрочанский'}</p>
+    <p>Platform: ${userId || 'нет id'}</p>
+    <p>Platform: ${userIsBot || 'не bot'}</p>
+    <p>Platform: ${userLanguageCode || 'марсианин'}</p>
+    <p>Platform: ${userIsPremium || 'не Premium'}</p>
+    <img src="${userPhoto}" alt="photo" width="200" height="200">
+    <p>Platform: ${userPlatform || 'anonymous'}</p>
     
     <h2>Telegram Web App</h2>
     <div class="card">
