c9f8494aa2c74b5820872b536ae7a30261959476
AlexburtT <alexburt12634@gmail.com>
2024-07-23 14:00:15 +0900

    update date time

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import "./assets/main.css";
 WebApp.ready();
 
 const nameUser = WebApp.initDataUnsafe.user?.first_name;
-const userDateRegister = WebApp.initDataUnsafe.auth_date.toLocaleString();
+const userDateRegister: number = parseInt(WebApp.initDataUnsafe.auth_date.toLocaleString());
 
-console.log(`Пользователь ${nameUser} зарегистрировался ${userDateRegister}`);
+let convertDate = new Date(userDateRegister * 1000);
+let utcString = convertDate.toUTCString();
+let time = utcString.slice(17, 25);
+let date = utcString.slice(0, 16);
+let timeDate = `${date} ${time}`;
+
+console.log(`Пользователь ${nameUser} зарегистрировался ${timeDate}`);
 
 document.querySelector("h1")!.innerHTML = `Привет, ${nameUser || "путник"}!`;
