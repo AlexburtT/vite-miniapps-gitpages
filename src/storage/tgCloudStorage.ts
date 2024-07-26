1c7fe227664bd662409b8294197faf701c1151f9
AlexburtT <alexburt12634@gmail.com>
2024-07-26 16:22:40 +0900

    up

--- src/storage/tgCloudStorage.ts
+++ src/storage/tgCloudStorage.ts
@@ -1,21 +1,21 @@
 import WebApp from "@twa-dev/sdk";
 
- // Облачное хранилище (ключ, значение)
+// Облачное хранилище (ключ, значение)
 export const setCloudStorage = (key: string, value: string) => {
-    new Promise((resolve) => {
-        WebApp.CloudStorage.setItem(key, value, () => {
-            resolve(value);
-        });
-    })
-}; 
-
-export const getCloudStorage = (key: string, defaultValue) => {
-    new Promise((resolve) => {
-        WebApp.CloudStorage.getItem(key, ((_, value) => {
-            if (value === null) {
-                resolve(defaultValue);
-            }
-            resolve(value);
-        }));
-    })
+  new Promise((resolve) => {
+    WebApp.CloudStorage.setItem(key, value, () => {
+      resolve(value);
+    });
+  });
 };
+
+export const getCloudStorage = (key: string, defaultValue: string) =>
+  new Promise((resolve) => {
+    WebApp.CloudStorage.getItem(key, (value) => {
+      if (value === null) {
+        resolve(defaultValue);
+      } else {
+        resolve(value);
+      }
+    });
+  });
